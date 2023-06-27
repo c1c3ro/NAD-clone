090ac05dc15e68bf3f759caa96eb431ce2610f0a
Cícero José <49200311+c1c3ro@users.noreply.github.com>
2023-06-27 14:46:49 -0300

    page finished

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-
-// window.onload = function() {
-//     headerHeight = document.getElementById("header").offsetHeight;
-//     document.getElementById("spacer").style.height = headerHeight + "px";
-// };
-
-
-window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
 
-    const navItems = document.querySelectorAll(".nav-link");
-
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.backgroundColor = "#4b0412";
-        document.getElementById("navbar-logo").src = "https://cdn.nad-edu.com.br/wp-content/uploads/2020/02/logo_oficial_branca_compress.png"
-        document.getElementById("navbar-toggle").style.color = "#fff";
+        $("#header").css("background-color", "#4b0412");
+        $("#navbar-logo").attr("src", "https://cdn.nad-edu.com.br/wp-content/uploads/2020/02/logo_oficial_branca_compress.png");
+        $("#navbar-toggle").css("color", "#fff");
         if (screen.availWidth > 992){
-            for (const navItem of navItems) {
-                navItem.classList.add('scroll');
-            }
+            $(".nav-link").addClass("scroll");
         }
-        
     } else {
-        document.getElementById("header").style.backgroundColor = "#fff";
-        document.getElementById("navbar-logo").src = "https://cdn.nad-edu.com.br/wp-content/uploads/2021/07/Logo-vermelha-230x90.png"
-        document.getElementById("navbar-toggle").style.color = "#4b0412";
+        $("#header").css("background-color", "#fff");
+        $("#navbar-logo").attr("src", "https://cdn.nad-edu.com.br/wp-content/uploads/2021/07/Logo-vermelha-230x90.png");
+        $("#navbar-toggle").css("color", "#4b0412");
         if (screen.availWidth > 992){
-            for (const navItem of navItems) {
-                navItem.classList.remove('scroll');
-            }
+            $(".nav-link").removeClass("scroll");
         }
     }
 }
+
+$(function() {
+    window.onscroll = function() {scrollFunction()};
+});
